c9a0fe820948a90ff0fd33cd39a17fa2cc7585e1
SidKalje <sidkalje123@gmail.com>
2023-04-16 00:02:17 -0700

    everything

---
+++ VolunteerHub/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import LoginPage from "./Components/LoginPage";
+import "./App.css";
+
+function App() {
+  return;
+  <>
+    <LoginPage />
+  </>;
+}
+
+export default App;
