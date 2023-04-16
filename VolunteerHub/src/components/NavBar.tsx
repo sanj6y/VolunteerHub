78c65013df324d93de8c6de085cd93df2a84fba4
SidKalje <sidkalje123@gmail.com>
2023-04-16 04:49:16 -0700

    asdf

--- VolunteerHub/src/components/NavBar.tsx
+++ VolunteerHub/src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import styles from "./NavBar.css";
+import styles from "./NavBar.module.css";
 import { Container } from "react-bootstrap";
 
 const NavBar = () => {
